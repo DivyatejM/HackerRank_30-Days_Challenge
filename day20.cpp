635d812cfb2836953c770a6c85f66f434586d5c5
Divyatej Mishra <76866090+DivyatejM@users.noreply.github.com>
2021-08-05 19:28:43 +0530

    Add files via upload

---
+++ day20.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int i, j,  temp, count=0, Swaps;
+    // cin>>n;
+    // int array[n];
+    
+    // for (i = 0; i <n; i++) 
+    // {
+    //     cin>>array[i];
+    // } 
+    for (i = n-1; i >0; i--) 
+    {
+        Swaps = 0;
+    for (j = 0; j <i; j++) 
+    {           
+        if(a[j]>a[j+1])
+        {
+            temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+            Swaps++;
+            count++;
+        }
+    }
+    if (Swaps == 0) 
+    {
+        break;
+    }
+    }   
+    cout<<"Array is sorted in "<<count<<" swaps.\n";
+    cout<<"First Element: "<<a[0]<<endl;
+    cout<<"Last Element: "<<a[n-1]<<endl;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
