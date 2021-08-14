218f14cc812cf76103c54672e5dc9cad45b521fc
Divyatej Mishra <76866090+DivyatejM@users.noreply.github.com>
2021-08-14 18:41:56 +0530

    Create day0.cpp

---
+++ day0.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    string input_string; 
+    getline(cin, input_string); 
+    cout << "Hello, World." << endl;
+    cout<<input_string;
+    return 0;
+}
